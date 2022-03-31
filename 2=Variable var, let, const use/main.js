5cba4e8b96bbc625e9c4c78257e545cd87b2e62f
zumonhossain <zumon15-13169@diu.edu.bd>
2022-03-31 18:21:07 +0600

    Variable var, let, const use

---
+++ 2=Variable var, let, const use/main.js
@@ -0,0 +1,11 @@
+var productName = 'Book';
+let productPrice = 50;
+const productQuantity = 5;
+const productIsAvailable = true;
+
+console.log(
+    'Product Name = ' + productName +
+    ' Product Price = ' + productPrice +
+    ' Product Quantity = ' + productQuantity +
+    ' Product is Available = ' + true
+)
