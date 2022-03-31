02a55b3a83e45332f082df00f5eea3d8dc49e5b8
zumonhossain <zumon15-13169@diu.edu.bd>
2022-03-31 18:49:58 +0600

    Array

---
+++ 4=Array/main.js
@@ -0,0 +1,14 @@
+
+
+const productInfo = ['Book', 30, 10, true];
+
+console.log(productInfo[0]);
+console.log(productInfo[1]);
+console.log(productInfo[2]);
+console.log(productInfo[3]);
+
+
+console.log(`
+    Product Name=${productInfo[0]} Product Price=${productInfo[1]} Product Quantity=${productInfo[2]} Product is Available=${productInfo[3]}
+`);
+
