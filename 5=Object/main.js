a8845c0973918dd4c17e67a8ecd053aaa002c3cd
zumonhossain <zumon15-13169@diu.edu.bd>
2022-04-01 09:52:37 +0600

    Object= Complex Data Type

---
+++ 5=Object/main.js
@@ -0,0 +1,24 @@
+
+
+const productInforObj = {
+    productName:'Book',
+    productPrice: 30,
+    productQuantity: 3,
+    productIsAvailable: true
+}
+
+console.log(productInforObj['productName'])
+console.log(productInforObj['productPrice'])
+
+console.log(productInforObj.productQuantity)
+console.log(productInforObj.productIsAvailable)
+
+
+
+
+console.log(`
+    Product Name=${productInforObj.productName} Product Price=${productInforObj.productPrice} Product Quantity=${productInforObj.productQuantity} Product is Available=${productInforObj.productIsAvailable}
+`);
+
+
+
