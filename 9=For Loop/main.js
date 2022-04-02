6f2b29e059d516b081105af6934d1ff2363fdcb0
zumonhossain <zumon15-13169@diu.edu.bd>
2022-04-02 10:19:13 +0600

    For Loop

---
+++ 9=For Loop/main.js
@@ -0,0 +1,23 @@
+
+
+// loop to avoid repeated action
+// for loop
+// while loop
+
+
+// for loop
+
+for(let i = 0; i <= 10; i++){
+    console.log(i);
+}
+
+
+// for loop daynamic array value show
+
+const productInfoArray = ['Book', 50, 5, true];
+console.log(productInfoArray.length)
+
+for(let i = 0; i < productInfoArray.length; i++){
+    console.log(productInfoArray[i]);
+}
+
