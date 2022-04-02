832b78f8d84f6f5c816372b44a885a049c848c42
zumonhossain <zumon15-13169@diu.edu.bd>
2022-04-02 10:31:25 +0600

    While Loop

---
+++ 10= While Loop/main.js
@@ -0,0 +1,23 @@
+
+
+// while loop
+
+// let i = 0;
+
+// while(i < 10){
+//     console.log(i);
+//     i++;
+// }
+
+
+
+// while loop daynamic array value show
+
+const productInfoArray = ['Book', 50, 5, true];
+
+let i = 0;
+
+while(i < productInfoArray.length){
+    console.log(productInfoArray[i]);
+    i++;
+}
