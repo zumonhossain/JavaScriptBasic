485b7d94364e752ac59d324ca5237d15812a9045
zumonhossain <zumon15-13169@diu.edu.bd>
2022-06-18 12:46:50 +0600

    Conditional Statement

---
+++ 21=Conditional Statement/main.js
@@ -0,0 +1,13 @@
+var a = 50;
+var b = 40;
+var c = 30;
+
+if(a > b && a > c){
+	console.log('A is the largest Number');
+}
+else if(b > a && b > c){
+	console.log('B is the largest Number');
+}
+else{
+	console.log('C is the largest Number');
+}
