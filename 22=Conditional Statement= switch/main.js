db120db00737644cfcebaf77954bfa0b9082cf85
zumonhossain <zumon15-13169@diu.edu.bd>
2022-06-18 13:11:01 +0600

    Switch Statement

---
+++ 22=Conditional Statement= switch/main.js
@@ -0,0 +1,27 @@
+var weekDay = 'Sunday';
+
+switch(weekDay){
+	case 'Saturday':
+		console.log('Saturday! Today is closed');
+		break;
+	case 'Sunday':
+		console.log('Sunday! It is normal day');
+		break;
+	case 'Monday':
+		console.log('Monday! It is normal day');
+		break;
+	case 'Tuesday':
+		console.log('Tuesday! It is normal day');
+		break;
+	case 'Wednesday':
+		console.log('Wednesday! It is normal day');
+		break;
+	case 'Thursday':
+		console.log('Thursday! It is normal day');
+		break;
+	case 'Friday':
+		console.log('Friday! It is normal day');
+		break;
+	default:
+		console.log('Not a Day');
+}
