86dd52fb18befaa2eb9549299d585e36dba4fe7a
zumonhossain <zumon15-13169@diu.edu.bd>
2022-06-20 11:57:22 +0600

    While Loop

---
+++ 25= While Loop/main.js
@@ -0,0 +1,9 @@
+var num = 10;
+
+do{
+    console.log('Number is:' + num);
+    num +=1;
+    // num ++; // i++ er poriborte +=1 likha jay etao 1 kore barbe
+}while(num < 20){
+    console.log("Outside The Loop");
+}
