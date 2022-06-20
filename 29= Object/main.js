566871cbbf038e997d56dda0a2586f063519e0ad
zumonhossain <zumon15-13169@diu.edu.bd>
2022-06-20 23:34:34 +0600

    object

---
+++ 29= Object/main.js
@@ -0,0 +1,14 @@
+
+
+
+var zumon = {
+    firstName: 'Zumon',
+    lastName: 'Hossain',
+    age: 23,
+    address: 'Dhaka'
+}
+
+// object a kono akta line pore add korte chaile ai code
+zumon.phoneNumber = '01852669486';
+
+console.log(zumon);
