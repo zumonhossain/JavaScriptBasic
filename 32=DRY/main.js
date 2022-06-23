1412f24b7a5b1648cb4f83b5a69efecab26f426e
zumonhossain <zumon15-13169@diu.edu.bd>
2022-06-23 11:35:12 +0600

    'dry'

---
+++ 32=DRY/main.js
@@ -0,0 +1,10 @@
+function addTwoNum(x, y){
+    var sum = x + y;
+    console.log('Sum of' + x + 'and' + y + 'is:' + sum);
+}
+
+addTwoNum(5, 5)
+addTwoNum(10, 10)
+addTwoNum(20, 50)
+addTwoNum(50, 10)
+addTwoNum(100, 10)
