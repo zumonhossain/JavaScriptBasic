911b4badf5abb5d3e17feeed2c2c35e82c811dd5
zumonhossain <zumon15-13169@diu.edu.bd>
2022-06-24 18:00:00 +0600

    Array Exercise

---
+++ 33= Array Exercise/main.js
@@ -0,0 +1,7 @@
+
+
+// I am Zumon Hossainand24.Iam a Web Developer.I love Javascript
+
+const profile = ['Zumon Hossain', 24, 'Web Developer', 'Javascript']
+
+console.log('I am' + ' ' + profile[0] + 'and' + profile[1] + '.' + 'Iam a' + ' ' + profile[2] + '.' + 'I love' + ' ' + profile[3])
