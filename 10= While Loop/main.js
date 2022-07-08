ee91d172e7a7b0471c0243ad8c5fe77d7d4b6eed
zumonhossain <zumon15-13169@diu.edu.bd>
2022-07-08 21:01:55 +0600

    <h1>Template Literal</h1>

--- 10= While Loop/main.js
+++ 10= While Loop/main.js
@@ -11,13 +11,68 @@
 
 
 
-// while loop daynamic array value show
+// Do While
 
-const productInfoArray = ['Book', 50, 5, true];
+// var num = 15;
 
-let i = 0;
+// do{
+//     console.log('num is :' + num);
+//     num += 1;
+// }while(num < 10);
+// console.log('Outside the loop');
 
-while(i < productInfoArray.length){
-    console.log(productInfoArray[i]);
-    i++;
-}
+
+
+// While Loop
+
+// var num = 10;
+
+// do{
+//     console.log('num is :' + num);
+//     num += 1;
+// }while(num < 20);
+// console.log('Outside the loop');
+
+
+// // while loop daynamic array value show
+
+// const productInfoArray = ['Book', 50, 5, true];
+
+// let i = 0;
+
+// while(i < productInfoArray.length){
+//     console.log(productInfoArray[i]);
+//     i++;
+// }
+
+// Break
+// for (var i = 0; i < 10; i++){
+//     if(i === 5){
+//         break;
+//     }
+//     console.log('i is now at: ' + i);
+// }
+// i is now at: 0
+// i is now at: 1
+// i is now at: 2
+// i is now at: 3
+// i is now at: 4
+
+
+// for(var i = 0; i < 10; i++){
+//     if(i === 5){
+//         console.log(i + 'is skipped');
+//         continue;
+//     }
+//     console.log('i is now at:' +i);
+// }
+// i is now at: 0
+// i is now at: 1
+// i is now at: 2
+// i is now at: 3
+// i is now at: 4
+// i is now at: 5
+// i is now at: 6
+// i is now at: 7
+// i is now at: 8
+// i is now at: 9
---
+++ 44=Template Literal/main.js
@@ -0,0 +1,5 @@
+let MyName = 'Zumon Hossain'
+let age = 24
+let work = 'Student'
+
+console.log(`My Name is ${MyName} and I\'m ${age} years old! Currently I\'m a ${work}`)
